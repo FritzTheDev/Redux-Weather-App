c1a0fb30578163c1c03e7ced28bebacf4898d3bf
ottovbismarck <fritzj96@gmail.com>
2018-08-03 14:24:13 -0700

    Got basic API requests to the Weather service going

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '6f404d71889b22666cd42b5aadc3cf3f';
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
