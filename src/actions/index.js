5be785538a0e7e8f6d33f20e4d2567696f2d25e1
ottovbismarck <fritzj96@gmail.com>
2018-08-03 13:59:24 -0700

    added API Root URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
 const API_KEY = '6f404d71889b22666cd42b5aadc3cf3f';
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: FETCH_WEATHER
+    };
+}
